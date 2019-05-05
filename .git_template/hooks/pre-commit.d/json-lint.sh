0673b04f871d82f1cbc61f9752d55b2cda88b4ad
Nick Lopez <nlopez@gmail.com>
2019-05-04 21:54:27 -0400

    NUL delimited filename fix

--- .git_template/hooks/pre-commit.d/json-lint.sh
+++ .git_template/hooks/pre-commit.d/json-lint.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 # Runs all .json or .js files through pythons json lint tool before commiting,
 # to make sure that you don't commit broken json objects.
-IFS=$'\n\t'
 
 git_dir=$(git rev-parse --show-toplevel)
-files="$(git diff-index --name-only --diff-filter=ACM --cached HEAD -- \
-  | grep -E '.*\.(json|js)$')"
 
-for file in $files; do
-  if ! python -mjson.tool "$file" 2> /dev/null ; then
+function get_files() {
+  git diff-index -z --name-only --diff-filter=ACM --cached HEAD -- \
+  | grep -E '.*\.(json|js)$'
+}
+
+while read -r -d $'\0'; do
+  if ! python -mjson.tool "$REPLY" 2> /dev/null ; then
     echo "Lint check of JSON object failed. Your changes were not commited."
-    echo "in $git_dir/$file:"
-    python -mjson.tool "$file"
+    echo "in $git_dir/$REPLY:"
+    python -mjson.tool "$REPLY"
     exit 1
   fi
-done
+done < <(get_files)
