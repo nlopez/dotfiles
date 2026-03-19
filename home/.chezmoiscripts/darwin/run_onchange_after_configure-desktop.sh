65e7ff91b637d2d2ff887c2102a49a7b5773bcc8
Nick Lopez <20211302+nlopez@users.noreply.github.com>
2026-03-18 20:57:44 -0400

    new mac things

---
+++ home/.chezmoiscripts/darwin/run_onchange_after_configure-desktop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -eufo pipefail
+
+osascript -e 'tell application "System Events" to tell every desktop to set picture to "/System/Library/Desktop Pictures/Solid Colors/Stone.png" as POSIX file'
