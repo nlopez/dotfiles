2c4db17ea2fe78581ebca639f46a9c3d96f7ffe4
Nick Lopez <20211302+nlopez@users.noreply.github.com>
2026-03-18 20:57:44 -0400

    new mac things

---
+++ home/.chezmoiscripts/darwin/run_onchange_after_configure-defaults.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -eufo pipefail
+
+# Keyboard
+defaults write -g InitialKeyRepeat -int 15
+defaults write -g KeyRepeat -int 2
+defaults write -g ApplePressAdndHoldEnabled -bool false
+# Use F1, F2, etc. keys as standard function keys
+defaults write -g com.apple.keyboard.fnState -bool true
+# Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)
+defaults write -g AppleKeyboardUIMode -int 2
+
+# Finder
+defaults write -g AppleShowAllExtensions -bool true
+defaults write -g AppleShowAllFiles -bool true
+defaults write com.apple.finder ShowPathbar -bool true
+# List view by default
+defaults write com.apple.finder FXPreferredViewStyle -string Nlsv
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
+# Search the current folder by default when performing a search in Finder
+defaults write com.apple.finder FXDefaultSearchScope -string SCcf
+# Don't show the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+# Save to disk (not iCloud) by default
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+killall Finder
+
+# Dock
+defaults write com.apple.dock orientation -string bottom
+defaults write com.apple.dock static-only -bool true
+defaults write com.apple.dock show-process-indicators -bool false
+defaults write com.apple.dock contents-immutable -bool true
+defaults write com.apple.dock size-immutable -bool true
+defaults write com.apple.dock position-immutable -bool true
+defaults write com.apple.dock show-recents -bool false
+defaults write com.apple.dock tilesize -int 64
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0
+
+killall Dock
