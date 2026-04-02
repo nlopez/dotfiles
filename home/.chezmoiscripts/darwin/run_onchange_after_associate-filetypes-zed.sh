bc6133be529af5642731e857051572e2e14fa45d
Nick Lopez <20211302+nlopez@users.noreply.github.com>
2026-04-02 16:52:13 -0400

    no zed autoupdates; x-e; filetypes

--- home/.chezmoiscripts/darwin/run_onchange_after_associate-filetypes-zed.sh
+++ home/.chezmoiscripts/darwin/run_onchange_after_associate-filetypes-zed.sh
@@ -3,6 +3,6 @@
 set -eufo pipefail
 
 # Associate common text/code filetypes with Zed
-for ext in txt json yaml sh xml sql go py; do
+for ext in txt json yaml sh xml sql go py js zsh rb; do
   duti -s dev.zed.Zed ."$ext" all
 done
