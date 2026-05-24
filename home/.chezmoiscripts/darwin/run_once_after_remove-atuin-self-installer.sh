d82a75ac49603be8c90e1fd17d33908c8bd5d7b1
Nick Lopez <20211302+nlopez@users.noreply.github.com>
2026-05-24 17:26:07 -0400

    remove fish shell

--- home/.chezmoiscripts/darwin/run_once_after_remove-atuin-self-installer.sh
+++ home/.chezmoiscripts/darwin/run_once_after_remove-atuin-self-installer.sh
@@ -5,15 +5,3 @@
 # Self-installed binary + logs
 rm -rf "$HOME/.atuin"
 
-# Remove ~/.atuin/bin from fish's persistent $fish_user_paths universal variable
-if command -v fish >/dev/null 2>&1; then
-  fish -c '
-    set -l clean
-    for p in $fish_user_paths
-      if test "$p" != "$HOME/.atuin/bin"
-        set -a clean $p
-      end
-    end
-    set -U fish_user_paths $clean
-  '
-fi
--- home/dot_config/fish/functions/pi.fish
+++
@@ -1,4 +0,0 @@
-function pi --wraps pi --description 'Run pi with AWS_PROFILE unset'
-    set --erase AWS_PROFILE
-    command pi $argv
-end
