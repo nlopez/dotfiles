9db1841ceed73df5549b051572314a0dc4fc16c4
Nick Lopez <20211302+nlopez@users.noreply.github.com>
2026-09-10 18:34:54 -0400

    hypr padding

--- home/dot_config/hypr/config/decorations.lua
+++ home/dot_config/hypr/config/decorations.lua
@@ -4,8 +4,8 @@ hl.config({
   general = {
     gaps_in = 3,
     gaps_out = 8,
-    -- border_size = 2,
-    -- extend_border_grab_area = 10,
+    border_size = 2,
+    extend_border_grab_area = 10,
     resize_on_border = true,
     col = {
       active_border = {
