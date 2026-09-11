c52c90328e7622071e197df880a5dce148b859b6
Nick Lopez <20211302+nlopez@users.noreply.github.com>
2026-09-11 07:25:06 -0400

    fmt, bitdepth 10

--- home/dot_config/hypr/config/monitors.lua
+++ home/dot_config/hypr/config/monitors.lua
@@ -1,5 +1,5 @@
 -- Monitor wiki https://wiki.hypr.land/Configuring/Basics/Monitors/
 
-hl.monitor({ output = MONITOR1, mode = "highrr", vrr = 0, scale = 1.25, position = "0x0",    })
-hl.monitor({ output = MONITOR2, mode = "highrr", vrr = 3, scale = 1.25, position = "2048x0", })
-hl.monitor({ output = MONITOR3, mode = "highrr", vrr = 0, scale = 1.25, position = "4800x0", })
+hl.monitor({ output = MONITOR1, mode = "highrr", vrr = 0, scale = 1.25, position = "0x0",    bitdepth = 10 })
+hl.monitor({ output = MONITOR2, mode = "highrr", vrr = 3, scale = 1.25, position = "2048x0", bitdepth = 10 })
+hl.monitor({ output = MONITOR3, mode = "highrr", vrr = 0, scale = 1.25, position = "4800x0", bitdepth = 10 })
--- home/dot_config/hypr/config/windowrules.lua
+++ home/dot_config/hypr/config/windowrules.lua
@@ -72,7 +72,7 @@ hl.window_rule({
 -- Opacity Overrides
 local terminals = "^(kitty|ghostty|[Kk]onsole|Alacritty|gnome-terminal|xfce[0-9]?-terminal)$"
 
-hl.window_rule({ match = { class = "^(firefox|zen)$" }, opacity = "1.0 override" })
+hl.window_rule({ match = { class = "^(firefox|zen|brave(-origin)?)$" }, opacity = "1.0 override" })
 hl.window_rule({ match = { class = terminals }, opacity = "1.0 override" }) -- Override opacity in favor of terminal settings for opacity. If your terminal doesn't support transparency, you can remove this rule.
 hl.window_rule({ match = { class = "^(mpv|org.kde.haruna|.*plex.*|org\\.kde\\.gwenview|.*vlc.*)$" }, opacity = "1.0 override" })
 
